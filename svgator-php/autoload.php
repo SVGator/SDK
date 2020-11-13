86680421b088c73a774f0121c561215c9bb99cde
Alexandru <alucaci@wayfareinteractive.com>
2020-11-13 14:03:27 +0200

    Updated autoloader

--- svgator-php/autoload.php
+++ svgator-php/autoload.php
@@ -1,25 +1,28 @@
 <?php
 /**
+ * Autoloader for SVGator's PHP SDK
  *
+ * https://www.svgator.com/
  */
-spl_autoload_register('SVGator_autoloader');
+spl_autoload_register('SVGatorSDK_autoloader');
 
 function SVGator_autoloader($className) {
 	$className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-	if(strpos($className, 'SVGator') >= 0) {
-		$classes_dir = __DIR__
-		               .DIRECTORY_SEPARATOR
-		               .'class'
-		               .DIRECTORY_SEPARATOR;
 
-		$class_file = $className.'.php';
-
-		if(file_exists($classes_dir.$class_file)) {
-			require_once $classes_dir.$class_file;
-
-			return true;
-		}
-
+	if(strpos($className, 'SVGatorSDK') === false) {
 		return false;
 	}
+
+	$classes_dir = __DIR__
+	               . DIRECTORY_SEPARATOR
+	               . 'class'
+	               . DIRECTORY_SEPARATOR;
+
+	$class_file = $className . '.php';
+
+	if(!file_exists($classes_dir . $class_file)) {
+		return false;
+	}
+
+	require_once $classes_dir . $class_file;
 }
