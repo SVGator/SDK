457f379ce84f3becd26376c983f1dcaaae0a3ee7
Kovago Lajos <lajos.kovago@svgator.com>
2026-06-24 10:19:40 +0300

    Added skeleton endpoint

--- svgator-backend/src/Projects.js
+++ svgator-backend/src/Projects.js
@@ -81,6 +81,36 @@ class Projects {
 
         return await this.inst.backend.get('/project', {access_token, project_id});
     }
+
+    async skeleton(access_token, project_id, options){
+        if (!this.inst.options.secret_key) {
+            throw new Error("options.secret_key is missing");
+        }
+
+        if (!access_token) {
+            throw new Error("access_token is missing");
+        }
+
+        if (!project_id) {
+            throw new Error("project_id is missing");
+        }
+
+        let args = {access_token, project_id};
+
+        if (options) {
+            if (options.depth !== undefined && options.depth !== null) {
+                args.depth = options.depth;
+            }
+            if (options.item) {
+                args.item = options.item;
+            }
+            if (options.fields) {
+                args.fields = options.fields;
+            }
+        }
+
+        return await this.inst.backend.get('/skeleton', args);
+    }
 }
 
 module.exports = Projects;
